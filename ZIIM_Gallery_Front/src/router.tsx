5c9ef4059397ec3312495ac0fddd96ae4331a562
caromain <romain.carrere@ynov.com>
2026-09-15 11:44:26 +0200

    feat(router): update Homepage route

--- ZIIM_Gallery_Front/src/router.tsx
+++ ZIIM_Gallery_Front/src/router.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/homepage";
 
 export default function Router(){
     return (
         <BrowserRouter>
             <Routes>
                 {/*
-                <Route path="*" element={<NotFound>} />
-                <Route path="/" element={<Home>} />
-                */}
+                <Route path="*" element={<NotFound />} /> */}
+                <Route path="/" element={<Homepage />} />
             </Routes>
         </BrowserRouter>
     )
