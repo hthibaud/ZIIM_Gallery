55928a4d31be1f43a53334eb2ed2d1878cb48a90
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2026-09-15 10:57:35 +0200

    feat(App): add Router component and background styling

--- ZIIM_Gallery_Front/src/App.tsx
+++ ZIIM_Gallery_Front/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Router from './router';
 
 function App() {
 
   return (
     <>
-      
+      <Router/>
+      <div className="bg-gray-900">
+
+      </div>
     </>
   )
 }
