e86cef57e00d760c26ba55ef86c2c9c24d92e617
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2026-09-15 12:26:47 +0200

    feat(user): create User page with UserPicture component

---
+++ ZIIM_Gallery_Front/src/pages/user.tsx
@@ -0,0 +1,10 @@
+import UserPicture from "../components/user/UserPicture";
+
+export default function User(){
+    return (
+        <>
+            <UserPicture id="" />
+            <h1>User</h1>
+        </>
+    );
+}
