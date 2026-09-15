5e9a94a119d3729ad3106ab781378b14971b5f60
hthibaud <thibaud.herry@ynov.com>
2026-09-15 11:18:33 +0200

    feat(HEADER): init header component

--- ZIIM_Gallery_Front/src/App.tsx
+++ ZIIM_Gallery_Front/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import HeaderComponent from './components/Header'
 
 function App() {
 
   return (
     <>
-      
+      <header>
+        <HeaderComponent />
+      </header>
     </>
   )
 }
---
+++ ZIIM_Gallery_Front/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function HeaderComponent(){
+    return (
+      <div className="h-20 bg-indigo-950">
+        <h1 className="p-4">
+          Hello, world!
+        </h1>
+      </div>  
+    )
+}
