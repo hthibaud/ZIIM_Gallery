a7b83ba562dae04198fd9186d0b177269554647d
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2026-09-15 10:57:41 +0200

    feat(router): add Router component with initial setup

---
+++ ZIIM_Gallery_Front/src/router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function Router(){
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/*
+                <Route path="*" element={<NotFound>} />
+                <Route path="/" element={<Home>} />
+                */}
+            </Routes>
+        </BrowserRouter>
+    )
+}
