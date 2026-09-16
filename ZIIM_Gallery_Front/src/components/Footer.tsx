6543255275db90c26799588f252cdf8d5ca376e2
hthibaud <thibaud.herry@ynov.com>
2026-09-16 09:50:28 +0200

    feat(FOOTER): added hover to button, small desc of the app

--- ZIIM_Gallery_Front/src/components/Footer.tsx
+++ ZIIM_Gallery_Front/src/components/Footer.tsx
@@ -1,12 +1,24 @@
 export default function FooterComponent() {
+
   return (
     <footer className="bg-gray-800 shadow-lg">
       <div className="max-w-7xl mx-auto h-32 flex justify-between items-center p-4">
         <h1 className="text-white text-4xl font-bold">ZIIM Gallery</h1>
 
-        <ul className="flex space-x-8 text-gray-400 text-lg font-medium hover:text-blue cursor-pointer">
+        <p className="text-gray-400">
+          Discover and follow art you love!
+        </p>
+
+        <ul className="flex space-x-8 font-medium cursor-pointer p-4">
           <li>
-            <a href="/logout">logout</a>
+            <a className="hover:text-gray-300 text-white" href="/support">
+              support
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-red-500 text-gray-400" href="/logout">
+              logout
+            </a>
           </li>
         </ul>
       </div>
