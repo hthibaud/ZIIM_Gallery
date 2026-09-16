9cbe70ec323c2aad4a4a937b13f1026f42405b59
caromain <romain.carrere@ynov.com>
2026-09-16 11:13:36 +0200

    feat(homepage): update Homepage component to include PaintCarousel

--- ZIIM_Gallery_Front/src/pages/homepage.tsx
+++ ZIIM_Gallery_Front/src/pages/homepage.tsx
@@ -1,7 +1,11 @@
 import Hero from '../components/home/hero';
+import PaintCarousel from '../components/home/paintCarousel';
 
 export default function Homepage() {
-    return (
-        <Hero />
-    );
+  return (
+    <main className="min-h-screen bg-neutral-950">
+      <Hero />
+      <PaintCarousel />
+    </main>
+  );
 }
