b761aae88509a386fb86dd01e0ab17ad1a21d252
hthibaud <thibaud.herry@ynov.com>
2026-09-16 09:51:15 +0200

    feat(HEADER): made the name bigger, added hover on the link buttons on the right

--- ZIIM_Gallery_Front/src/components/Header.tsx
+++ ZIIM_Gallery_Front/src/components/Header.tsx
@@ -1,23 +1,22 @@
 import SearchBar from "./utils/SearchBar";
-import { useState } from "react";
 
 export default function HeaderComponent() {
-
-
 
   return (
     <header className="bg-gray-800 shadow-lg">
       <div className="max-w-7xl mx-auto h-32 flex justify-between items-center p-4">
-        <h1 className="text-white text-4xl font-bold">ZIIM Gallery</h1>
+        <h1 className="text-white  text-6xl font-bold m-4 p-4">
+          ZIIM Gallery
+        </h1>
           <SearchBar 
           />
-        <ul className="flex space-x-8 text-white text-lg font-medium cursor-pointer">
+        <ul className="flex space-x-8 text-white text-lg font-medium cursor-pointer p-4">
           <li></li>
           <li>
-            <a href="/mainGallery">Main Gallery</a>
+            <a className="hover:text-gray-300" href="/mainGallery">Main Gallery</a>
           </li>
           <li>
-            <a href="/user/0">profile</a>
+            <a className="hover:text-gray-300" href="/user/0">profile</a>
           </li>
         </ul>
       </div>
