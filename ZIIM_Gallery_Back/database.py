e40da0d9b40ac33de118705768100d7ab6d1e7d9
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2026-09-17 11:07:26 +0200

    feat(database): implement database connection and session management

---
+++ ZIIM_Gallery_Back/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from dotenv import load_dotenv
+import os
+
+class Base(DeclarativeBase):
+    pass
+
+class Database:
+    def __init__(self):
+        load_dotenv()
+
+        self.db_url = os.getenv("DATABASE_URL")
+
+        self.engine = create_engine(self.db_url)
+
+        self.session_local = sessionmaker(engine=self.engine, class_=Session, autoflush=False)
+
+    def get_session(self) -> Session:
+        return self.session_local
