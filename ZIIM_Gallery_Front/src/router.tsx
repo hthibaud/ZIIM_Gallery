f1bd61150bf5488556b63c5e000512fc82917bb1
caromain <romain.carrere@ynov.com>
2026-09-21 09:58:34 +0200

    feat(router): add NotFound route to handle undefined paths

--- ZIIM_Gallery_Front/src/router.tsx
+++ ZIIM_Gallery_Front/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import User from "./pages/user";
 import AuthentificationPage from "./components/utils/Authentification";
+import NotFound from "./components/error/notFound";
 
 export default function Router(){
     return (
@@ -9,7 +10,8 @@ export default function Router(){
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/user/:id" element={<User />} />
-	            <Route path ="/authentification" element = {<AuthentificationPage />} />           
+	            <Route path ="/authentification" element = {<AuthentificationPage />} />
+                <Route path ="*" element = {<NotFound />} />           
             </Routes>
         </BrowserRouter>
     )
