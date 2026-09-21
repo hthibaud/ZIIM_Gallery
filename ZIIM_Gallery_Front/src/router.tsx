4a50881b2ffdba815b11ff00588cca87e7afebd2
caromain <romain.carrere@ynov.com>
2026-09-21 11:53:39 +0200

    fix(router): correct buy section route and component import

--- ZIIM_Gallery_Front/src/router.tsx
+++ ZIIM_Gallery_Front/src/router.tsx
@@ -5,18 +5,18 @@ import AuthentificationPage from "./components/utils/Authentification";
 import HeaderComponent from "./components/Header";
 import FooterComponent from "./components/Footer";
 import NotFound from "./components/error/notFound";
-import Buy from "./components/shop/buySection";
+import BuySection from "./components/shop/buySection";
 
 export default function Router(){
     return (
         <BrowserRouter>
                 <HeaderComponent />
                 <Routes>
-                    <Route path ="*" element = {<NotFound />} />    
                     <Route path="/" element={<Homepage />} />
                     <Route path="/user/:id" element={<User />} />
                     <Route path="/authentification" element={<AuthentificationPage />} />
-                    <Route path="/buy" element={<buySection />} />
+                    <Route path="/buy/:id" element={<BuySection />} />
+                    <Route path ="*" element = {<NotFound />} />
                 </Routes>
                 <FooterComponent />
         </BrowserRouter>
