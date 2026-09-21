f99e2a1c4473f214ee89de6670fab8d168f383cc
Léo Gaiguant--Anquit <leo.gaiguantanquit@ynov.com>
2026-09-21 11:13:13 +0200

    feat(user): add profile_picture field to user model

--- ZIIM_Gallery_Back/model.py
+++ ZIIM_Gallery_Back/model.py
@@ -13,6 +13,7 @@ class user(Base):
     password: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False)
     date: Mapped[str] = mapped_column(nullable=False)
+    profile_picture = mapped_column(String, nullable=True)
     gallery_id: Mapped[int] = mapped_column(
         ForeignKey("gallery.id"),
         nullable=True
